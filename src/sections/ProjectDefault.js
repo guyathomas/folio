7a373642e7bff71ec2b28b4de625b19e6c2db151
Guy <guythomas721@gmail.com>
2017-05-10 21:51:18 +1000

    Add parallax

--- src/sections/ProjectDefault.js
+++ src/sections/ProjectDefault.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 
 const Project = (props) => {
-	console.log('props in Project', props)
+  const imgStyl = {
+      background: 'url('+ props.project.screenshotURL + ')',
+      backgroundSize: 'cover',
+    }
     return (
       <div className={"project " + props.side}>
       	<div className="description">
       		<h2><a href={props.project.url} target="_blank">{props.project.title}</a></h2>
       		<h3>{props.project.shortsummary}</h3>
       		<div className="pipelist">{props.project.keywords.join(' | ')}</div>
-      	</div>
+      	  <div className="longdesc">{props.project.longdesc}</div>
+        </div>
       	<div className="visuals">
-      		<div className="screenshot">
-      			<img src={props.project.screenshotURL} />
+      		<div className="screenshot" style={imgStyl}>
       		</div>
       	</div>
       </div>
